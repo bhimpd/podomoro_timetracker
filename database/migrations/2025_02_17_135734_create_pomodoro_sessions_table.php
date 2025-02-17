4c7ab2c7eb7a0d965c0f40a7dd669a349b496a87
intuji-bhimlamichhane <bhim.lamichhane@intuji.com>
2025-02-17 19:49:09 +0545

    migrated the pomodoro sessions table  with required columns

---
+++ database/migrations/2025_02_17_135734_create_pomodoro_sessions_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pomodoro_sessions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('task_id')->constrained()->onDelete('cascade');
+            $table->enum('status', ['running', 'paused', 'completed', 'skipped'])->default('running');
+            $table->timestamp('start_time')->nullable();
+            $table->timestamp('end_time')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pomodoro_sessions');
+    }
+};

1e3ceab8de8de9501ed625a5542df99a29cab0ba
intuji-bhimlamichhane <bhim.lamichhane@intuji.com>
2025-02-18 21:30:59 +0545

    created login route and controller

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -18,10 +19,40 @@ class AuthController extends Controller
             'password' => Hash::make($request->password)
         ]);
 
-        // Return a success response
+
+        if(!$user){
+            return response()->json([
+                'message' => 'User registered failed'
+            ], 500);
+        }
+        else{
+            $token = $user->createToken('authToken')->plainTextToken;
+    
+            return response()->json([
+                'message' => 'User registered successfully',
+                'user' => $user,
+                'token' => $token
+            ], 201);
+        }
+        
+    }
+
+    public function login(LoginRequest $request)
+    {
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'message' => 'Invalid email or password'
+            ], 401);
+        }
+    
+        $token = $user->createToken('authToken')->plainTextToken;
+    
         return response()->json([
-            'message' => 'User registered successfully',
-            'user' => $user
-        ], 201);
+            'message' => 'Login successful',
+            'user' => $user,
+            'token' => $token
+        ], 200);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -8,9 +8,11 @@ use Illuminate\Support\Facades\Route;
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
 
-// Route::middleware(['auth:sanctum'])->group(function (){
+Route::middleware(['auth:sanctum'])->group(function (){
 
-// });
+    Route::post('logout', [AuthController::class, 'logout']);
+
+});
 
 
 Route::get('/user', function (Request $request) {
